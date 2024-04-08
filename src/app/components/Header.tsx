dd58785e71222ec2cb853e52a7cde6a63abf55b8
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-08 19:10:15 +0800

    modified header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import image from "/public/portfolio_image.png";
+import portfolio_image from "/public/portfolio_image.png";
 
 export default function Header() {
   return (
     <main>
     <div className="mt-8 mx-12">
     <Image
-      src={image}
+      src={portfolio_image}
       width={155}
       alt="Picture of portfolio text"
     />
