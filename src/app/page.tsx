e4e7702cc95f446612c6689bda76f10b6a935372
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-08 18:02:43 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold underline mt-7 mx-11">
+      Portfolio
+    </h1>
+    </main>
+  );
+}
