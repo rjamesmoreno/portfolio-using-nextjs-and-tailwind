d227286ccc21b788dd88e62e11b46c6a8fb958da
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-12 11:34:20 +0800

    chore: removed main tag

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,14 +3,12 @@ import portfolio_image from "/public/portfolio_image.png";
 
 export default function Header() {
   return (
-    <main>
     <div className="mt-8 mx-12">
-    <Image
-      src={portfolio_image}
-      width={155}
-      alt="Picture of portfolio text"
-    />
+      <Image
+        src={portfolio_image}
+        width={155}
+        alt="Picture of portfolio text"
+      />
     </div>
-    </main>
   );
 }
