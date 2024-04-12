01522c91a3394f62b31a1696a746c2f3f4ba8e05
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-12 10:19:50 +0800

    added font

--- src/app/components/ImageCarousel.tsx
+++ src/app/components/ImageCarousel.tsx
@@ -7,12 +7,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import utilitycrate1 from "/public/utilitycrate1.png";
-import utilitycrate2 from "/public/utilitycrate2.png";
-import utilitycrate3 from "/public/utilitycrate3.png";
-import typhoonista1 from "/public/typhoonista1.png";
-import typhoonista2 from "/public/typhoonista2.png";
-import { group } from "console";
+
 
 type ImageCarouselProps = {
     imageList : StaticImageData[]
@@ -20,17 +15,17 @@ type ImageCarouselProps = {
 }
 
 export default function ImageCarousel({imageList, groupType} : ImageCarouselProps) {
-    return (<Carousel className= {groupType==='utilitycrate' ? "max-w-xs mx-auto" : "w-[50vw] mx-auto"}>
+    return (<Carousel className= {groupType==='utilitycrate' ? "w-[20vw] mx-auto" : "w-[50vw] mx-auto"}>
     <CarouselContent>
     {imageList.map((image) => {
         return (<CarouselItem>
             <div className="p-1">
               <Card>
                 <CardContent className="flex items-center justify-center p-6">
-                  <div className="border-solid border-2 border-black">
+                  <div>
                     <Image
                       src={image}
-                      width={groupType === 'utilitycrate' ? 400 : 1000}
+                      width={groupType === 'utilitycrate' ? 600 : 1000}
                       alt=""
                     />
                   </div>
